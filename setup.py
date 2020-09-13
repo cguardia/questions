74c819042110ad7093d2c8d1b23f8af792eec662
Carlos de la Guardia <cguardia@yahoo.com>
2020-09-12 19:00:23 -0500

    require at least Python 3.6

--- setup.py
+++ setup.py
@@ -19,14 +19,13 @@ test_requirements = ['pytest>=3', ]
 setup(
     author="Carlos de la Guardia",
     author_email='cguardia@yahoo.com',
-    python_requires='>=3.5',
+    python_requires='>=3.6',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
